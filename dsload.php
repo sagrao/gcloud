b459bb2a1d8c0683234b8a5aad5eacafd6b14357
sagrao <40746707+sagrao@users.noreply.github.com>
2018-07-02 23:48:37 -0500

    Add files via upload

---
+++ dsload.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+use Google\Auth\ApplicationDefaultCredentials;
+use Google\Cloud\Datastore\DatastoreClient;
+try
+{
+	$projectId = 'sagar-autocomplete2';
+	$datastore = new DatastoreClient([
+	    'projectId' => $projectId
+	]);
+	if (sizeof($argv) == 3) {
+		$transaction = $datastore->transaction();
+		$key = $datastore->key('SKU', $argv[1]);
+		$product = $datastore->entity( $key, [
+			'name' => strtolower($argv[2])
+		]);
+		$datastore->upsert($product);
+		$transaction->commit();
+	}
+} catch (Exception $e) {
+	echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+?>
